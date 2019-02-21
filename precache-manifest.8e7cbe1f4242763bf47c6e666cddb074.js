73b4645bf51bc3c6375e36d61af5d0023e58be6b
Anthony Kim <anthony011@gmail.com>
2019-02-20 20:15:39 -0500

    init commit

---
+++ precache-manifest.8e7cbe1f4242763bf47c6e666cddb074.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "909c79e52fd6c2b2b961",
+    "url": "/static/css/main.342149ca.chunk.css"
+  },
+  {
+    "revision": "909c79e52fd6c2b2b961",
+    "url": "/static/js/main.909c79e5.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "b2f5ab1217bac76f1d55",
+    "url": "/static/js/2.b2f5ab12.chunk.js"
+  },
+  {
+    "revision": "78e50f8cfa7b01479499f039909a37ad",
+    "url": "/index.html"
+  }
+];
